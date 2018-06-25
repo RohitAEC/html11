2d91c48e671d49442f923ffb7f87c0cfeace431b
RohitAEC <39111555+RohitAEC@users.noreply.github.com>
2018-06-26 00:04:25 +0530

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+<html>
+ <body>
+   
+     <script>
+              function myFunction() {
+                   document.getElementById("demo").innerHTML="Paragraph Changed.";
+                                    }
+     </script>   
+ </body>
+</html>
